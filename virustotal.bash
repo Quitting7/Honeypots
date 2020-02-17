5ec60f7636ad0c1cc7baab702006dc8c630368ab
Quitting7 <Franz.Kafque@gmail.com>
2020-02-17 13:46:52 +0100

    drive

--- virustotal.bash
+++ virustotal.bash
@@ -1,5 +1,32 @@
 #!/bin/bash
 
-for bin in $(ls -1 $HOME/binaries); do
+if [ "$#" -lt 2 ]; then
+	echo "usage: ./virustotal.py <send|recv> <api-key>"; exit 1
+fi
 
-done
+API_KEY=$2
+binpath=/home/money/binaries
+logpath=/home/money/
+#logpath=/home/money/log
+logfile=virustotal.json
+
+if [ "$1" == 'send' ]; then
+
+	for bin in $(ls -1 $binpath); do
+		if ! $(echo $bin | grep -q "\.tmp" ); then
+		# Only submit a report for each file with an md5sum name
+			./virustotal.py $binpath/$bin $API_KEY
+		fi
+	done
+
+elif [ "$1" == 'recv' ]; then
+
+	for bin in $(ls -1 $binpath); do
+		if ! $(echo $bin | grep -q "\.tmp" ); then
+
+			./virustotal.py $binpath/$bin $API_KEY fetch >> $logpath/$logfile 
+		fi
+	done
+else
+	echo "usage: virustotal.py <send|recv> <api-key>"
+fi
--- virustotal.py
+++ virustotal.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python3
-
 from sys import argv
 from time import sleep
+from os import path
 import json    
 import requests
 
-
-
 if len(argv) < 3:
-    print("usage: ./virustotal.py <file> <api-key> <fetch-only>"); exit(1)
+	print("usage: ./virustotal.py <file> <api-key> <fetch-only>"); exit(1)
 
-#---------- ANALYZE -----------#
 if len(argv) < 4:
+	#---------- ANALYZE -----------#
+	url='https://www.virustotal.com/vtapi/v2/file/scan'
 
-    url='https://www.virustotal.com/vtapi/v2/file/scan'
+	params = {'apikey': '<apikey>'}
 
-    params = {'apikey': '<apikey>'}
+	files = {'file': (path.basename(argv[1]), open(argv[1], 'rb'))}
 
-    files = {'file': (argv[1], open(argv[1], 'rb'))}
+	response = requests.post(url, files=files, params={'apikey':argv[2] })
 
-    response = requests.post(url, files=files, params={'apikey':argv[2] })
+	print(response.json())
 
-    print(response.json())
+	#wait = 10
+	#print("Sleeping for {} seconds before fetching report...".format(wait))
+	#sleep(wait)
 
-    wait = 10
-    print("Sleeping for {} seconds before fetching report...".format(wait))
-    sleep(wait)
+elif len(argv) >= 4:
+	#---------- GET Report ------------#
+	url = 'https://www.virustotal.com/vtapi/v2/file/report'
+	response = requests.get(url, params={ 'apikey': argv[2], 'resource': path.basename(argv[1])  }  )
 
-#---------- GET Report ------------#
-url = 'https://www.virustotal.com/vtapi/v2/file/report'
-response = requests.get(url, params={ 'apikey': argv[2], 'resource': argv[1]  }  )
+	obj = response.json()
+	
+	if "scan_data" in obj.keys():
 
-obj = response.json()
+		#---------- FORMATTING ----------#
+		wanted_top_fields = [ "scan_date", "resource", "total", "positives" ]
 
-#---------- FORMATTING ----------#
-wanted_top_fields = [ "scan_date", "resource", "total", "positives" ]
+		#with open(argv[1],"r") as infile:
+		#    data = infile.read()
+		#obj = json.loads(data)
 
-#with open(argv[1],"r") as infile:
-#    data = infile.read()
-#obj = json.loads(data)
+		pruned_obj = {}
 
-pruned_obj = {}
+		for field in wanted_top_fields:
+			pruned_obj[field] = obj[field]
 
-for field in wanted_top_fields:
-    pruned_obj[field] = obj[field]
+		pruned_obj["results"] = []
 
-pruned_obj["results"] = []
+		for field in obj["scans"]:
+			if obj["scans"][field]["result"]:
+				pruned_obj["results"].append(obj["scans"][field]["result"])
 
-for field in obj["scans"]:
-    if obj["scans"][field]["result"]:
-        pruned_obj["results"].append(obj["scans"][field]["result"])
-
-print(pruned_obj)
+		print(pruned_obj)
+	else:
+		print('{ "error": "Insufficent response from server"}'); exit(1)
